63f857f9e3a97a71e1ee5c5aeafb695bdea8ed08
SamWijnsma <samwijnsma@outlook.com>
2021-09-07 16:03:12 +0200

    Created tables connected to product

---
+++ database/factories/ManufacturerFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\manufacturer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ManufacturerFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = manufacturer::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
