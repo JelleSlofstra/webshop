26d111dbdc893b22a0f873a5e2d86985c3469b9f
SamWijnsma <samwijnsma@outlook.com>
2021-09-09 12:34:09 +0200

    cart en stock migrations

---
+++ database/factories/StockFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\stock;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class StockFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = stock::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
