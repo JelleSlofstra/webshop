30dd5d0cd91fb3b0b7d7698664172dc26e2fe60f
JelleSlofstra <jelleslofstra@gmail.com>
2021-09-10 10:29:46 +0200

    added the seeder content vor productvariant

--- database/seeders/ProductVariantSeeder.php
+++ database/seeders/ProductVariantSeeder.php
@@ -2,9 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
+use App\Models\ProductColour;
+use App\Models\ProductGender;
+use App\Models\ProductVariant;
+use App\Models\ProductSize;
 use Illuminate\Database\Seeder;
 
-class ProductVariantSeeder extends Seeder
+class ProductPropertySeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,6 +18,29 @@ class ProductVariantSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $products = Product::all();
+        $genders = ProductGender::all();
+        $sizes = ProductSize::all();
+        $colours = ProductColour::all();
+        
+        foreach ($products as $product)
+        {
+            foreach ($genders as $gender)
+            {
+                foreach ($sizes as $size)
+                {
+                    foreach ($colours as $colour)
+                    {
+                        ProductVariant::create([
+                            'product_id'        => $product->id,
+                            'product_size'      => $size->id,
+                            'product_colour'    => $colour->id,
+                            'product_gender'    => $gender->id,
+                        ]);
+                    }
+                }
+            }
+        }        
     }
 }
+
