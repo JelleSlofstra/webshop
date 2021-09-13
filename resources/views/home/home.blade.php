d7a6c580778ea870dee2d06a975d560157523745
SamWijnsma <samwijnsma@outlook.com>
2021-09-13 16:19:33 +0200

    $table verwijzing in models + home.blade view

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-2">
+            Hier komen de filters
+        </div>
+        <div class="col-md-10">
+            <div class="row justify-content-center">
+                @foreach ($categories as $category)
+                    <div class="col-md-6">
+                        <div class="card">
+                            <img src="" class="card-img-top rounded" alt="...">                                               
+                            
+                            <div class="card-body">
+                                <h3 class="card-title">{{$category->name}}</h3> 
+        <a href="{{ Route('categories.show', $category->id) }}">
+        <button>Naar alle {{$category->name}}</button>
+        </a> 
+                            </div>
+
+                        </div>
+                    </div>
+                   
+                @endforeach
+
+                
+            </div>
+        </div>           
+    </div>
+</div>
+
+@endsection
