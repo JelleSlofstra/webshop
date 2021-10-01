3571c2ac8f59bd55f6e8f4db4bff78be94b17dd3
JelleSlofstra <jelleslofstra@gmail.com>
2021-10-01 15:38:04 +0200

    cart responsiveness after ajax call

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -43,19 +43,21 @@ class Cart extends Model
                 $variant = ProductVariant::find($variantId);
                 $html .= '<div class="col-12 card my-3 p-4"><div class="row">';
                 //plaatje en buttons 
-                $html .= '<div class="col-lg-3 col-md-6 my-auto">';
+                $html .= '<div class="col-lg-3 col-md-4 my-auto">';
                 $html .= '<img src="../images/' . $variant->product->productImages->first->image->image . '" alt="" class="cart-img d-block mx-auto my-2 rounded">';
                 $html .= '<span class="d-block"> <button id="add-one" pv_id="' . $variantId . '" class="d-inline btn btn-primary update-cart">+</button> ';
                 $html .= '<button id="remove-one" pv_id="' . $variantId . '" class="d-inline btn btn-primary update-cart">-</button> ';
                 $html .= '<button id="remove-all" pv_id="' . $variantId . '" class="d-inline btn btn-primary update-cart"><i class="far fa-trash-alt"></i></button></span></div>';
                 //model
-                $html .= '<div class="col-lg-3 col-md-6 my-auto"><div class="my-3"><strong>Model: </strong></div>';
+                $html .= '<div class="col-md-4 col-lg-6 my-auto"><div class="row">';
+                $html .= '<div class="col-lg-6 col-md-12 my-auto"><div class="my-3"><strong>Model: </strong></div>';
                 $html .= '<div>' .$quantity . 'x ' . $variant->product->name . '</div></div>';
                 //opties
-                $html .= '<div class="col-lg-3 col-md-6 my-auto"><div class="my-3"><strong>Opties: </strong></div>';
+                $html .= '<div class="col-lg-6 col-md-12 my-auto"><div class="my-3"><strong>Opties: </strong></div>';
                 $html .= '<div>'.$variant->size->name.', '.$variant->colour->name.', '.$variant->gender->name.'</div></div>';
+                $html .= '</div></div>';
                 //prijzen
-                $html .= '<div class="col-lg-3 col-md-6 my-auto"><div class="my-3"><strong> Prijs excl btw ('.$variant->product->vatPercentage().')%:</strong> &euro; '.$variant->product->price.'</div>';
+                $html .= '<div class="col-lg-3 col-md-4 my-auto"><div class="my-3"><strong> Prijs excl btw ('.$variant->product->vatPercentage().')%:</strong> &euro; '.$variant->product->price.'</div>';
                 $html .= '<div><strong>Totaalprijs voor ' . $quantity . ' stuk(s):</strong> &euro; ' . $variant->product->VatIncPrice($quantity) .'</div></div>';
                 $html .= '</div></div>';
             } 
